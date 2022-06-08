875f01221279b36a9fb552050849c0c38c18515f
ilya-sakalouski <ilya6697089@mail.ru>
2022-06-08 11:48:33 +0300

    Refactor Task2

--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -3,25 +3,18 @@ package src.task2;
 public class Task2 {
     public static void informationCheck(String login, String password, String confirmPassword) {
         try {
-            if ((login.length() > 20) || (login.matches(" "))) {
+            if ((login.length() > 20) || (login.matches(" ")))
                 throw new WrongLoginExeption();
-            }
+
+            if ((password.length() > 20) || (password.matches(" ") || (password.matches("[0-9]"))))
+                throw new WrongPasswordExeption();
+
+            if (!password.equals(confirmPassword))
+                throw new WrongPasswordExeption();
+
         } catch (WrongLoginExeption e) {
             e.getMessage();
             throw new RuntimeException(e);
-        }
-        try {
-            if ((password.length() > 20) || (password.matches(" ") || (password.matches("[0-9]")))) {
-                throw new WrongPasswordExeption();
-            }
-        } catch (WrongPasswordExeption e) {
-            e.getMessage();
-            throw new RuntimeException(e);
-        }
-        try {
-            if (!password.equals(confirmPassword)) {
-                throw new WrongPasswordExeption();
-            }
         } catch (WrongPasswordExeption e) {
             e.getMessage();
             throw new RuntimeException(e);
